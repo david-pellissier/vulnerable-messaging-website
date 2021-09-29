5d56366d0ae1cba6b7bbe7fb151e2784fb403ee9
Nicolas Ogi <nicolas.ogi@heig-vd.ch>
2021-09-29 17:55:06 +0200

    Adding html and css

--- site/html/users.php
+++ site/html/users.php
@@ -1,37 +1,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <style>
-        table {
-            font-family: arial, sans-serif;
-            border-collapse: collapse;
-            width: 100%;
-        }
-
-        td, th {
-            border: 1px solid #dddddd;
-            text-align: left;
-            padding: 8px;
-        }
-
-        tr:nth-child(even) {
-            background-color: #dddddd;
-        }
-    </style>
+    <link href="http://getbootstrap.com/examples/jumbotron-narrow/jumbotron-narrow.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 </head>
 <body>
+<div class="container bootstrap snippets bootdey">
 
-<h2>HTML Table</h2>
+    <div class="header">
+        <h3 class="text-muted prj-name">
+            <span class="fa fa-users fa-2x principal-title"></span>
+            Users
+        </h3>
+    </div>
 
-<table>
-    <tr>
-        <th>Username</th>
-        <th>Valid account</th>
-        <th>Role</th>
-        <th>Actions</th>
-    </tr>
 
-</table>
+    <div class="jumbotron list-content">
+        <ul class="list-group">
+            <li href="#" class="list-group-item text-left">
+                <label class="name">
+                    Juan guillermo cuadrado<br>
+                </label>
+                <label class="pull-right">
+                    <a  class="btn btn-success btn-xs glyphicon glyphicon-ok" href="#" title="View">Edit</a>
+                    <a  class="btn btn-danger  btn-xs glyphicon glyphicon-trash" href="#" title="Delete">Delete</a>
+                </label>
+                <div class="break"></div>
+            </li>
+        </ul>
+        <a  class="btn btn-success btn-xs glyphicon glyphicon-ok" href="#" title="View">Add user</a>
+    </div>
+</div>
+</div>
 
 </body>
 </html>
