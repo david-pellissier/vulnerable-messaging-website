7c0c619362460045ce5bb79c0406d6d8d238d30c
david-pellissier <dav.pellissier@gmail.com>
2021-10-08 11:24:27 +0200

    Sidebar & modifs CSS

--- site/html/view/usercreation.php
+++ site/html/view/usercreation.php
@@ -2,53 +2,65 @@
 <html lang="en">
 <head>
     <title>Create a new user</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="css/message.css">
 </head>
 <body>
-<?php require 'view/components/info_to_user.php'?>
-<div class="container px-5 my-5">
-    <div class="row justify-content-center">
-        <div class="col-lg-8">
-            <div class="card border-0 rounded-3 shadow-lg">
-                <div class="card-body p-4">
-                    <div class="text-center">
-                        <div class="h1 fw-light">Create a new user</div>
+<?php require 'view/components/info_to_user.php' ?>
+<div class="root">
+    <?php require 'view/components/sidebar.php' ?>
+    <div class="container px-5 my-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="card border-0 rounded-3 shadow-lg">
+                    <div class="card-body p-4">
+                        <div class="text-center">
+                            <div class="h1 fw-light">Create a new user</div>
+                        </div>
+                        <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="username" type="text" placeholder="Username"
+                                       data-sb-validations="required"/>
+                                <label for="username">Username</label>
+                            </div>
+
+                            <select class="form-select" aria-label="Roles">
+                                <option value="1">Collaborator</option>
+                                <option value="2">Administrator</option>
+                            </select>
+                            <br>
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="password" type="password" placeholder="Password"
+                                       data-sb-validations="required"/>
+                                <label for="password">Enter new password</label>
+                                <div class="invalid-feedback" data-sb-feedback="password:required">New password is
+                                    required.
+                                </div>
+                            </div>
+
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="confpassword" type="password"
+                                       placeholder="Confirm password" data-sb-validations="required"/>
+                                <label for="confpassword">Confirm new password</label>
+                                <div class="invalid-feedback" data-sb-feedback="confpassword:required">Confirm new
+                                    password.
+                                </div>
+                            </div>
+
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                                <label class="form-check-label" for="defaultCheck1">Account validity</label>
+                            </div>
+
+                            <div class="d-grid">
+                                <button class="btn btn-primary btn-lg" id="submitButton" type="submit">Create</button>
+                            </div>
+                        </form>
                     </div>
-                    <form id="contactForm" data-sb-form-api-token="API_TOKEN">
-
-                        <div class="form-floating mb-3">
-                            <input class="form-control" id="username" type="text" placeholder="Username" data-sb-validations="required"/>
-                            <label for="username">Username</label>
-                        </div>
-
-                        <select class="form-select" aria-label="Roles">
-                            <option value="1">Collaborator</option>
-                            <option value="2">Administrator</option>
-                        </select>
-                        <br>
-                        <div class="form-floating mb-3">
-                            <input class="form-control" id="password" type="password" placeholder="Password" data-sb-validations="required" />
-                            <label for="password">Enter new password</label>
-                            <div class="invalid-feedback" data-sb-feedback="password:required">New password is required.</div>
-                        </div>
-
-                        <div class="form-floating mb-3">
-                            <input class="form-control" id="confpassword" type="password" placeholder="Confirm password" data-sb-validations="required" />
-                            <label for="confpassword">Confirm new password</label>
-                            <div class="invalid-feedback" data-sb-feedback="confpassword:required">Confirm new password.</div>
-                        </div>
-
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
-                            <label class="form-check-label" for="defaultCheck1">Account validity</label>
-                        </div>
-
-                        <div class="d-grid">
-                            <button class="btn btn-primary btn-lg" id="submitButton" type="submit">Create</button>
-                        </div>
-                    </form>
                 </div>
             </div>
         </div>
--- site/html/view/users.php
+++ site/html/view/users.php
@@ -2,34 +2,39 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <title>Users</title>
 </head>
 <body>
-<?php require 'view/components/info_to_user.php'?>
-<div class="container bootstrap snippets bootdey">
-    <div class="header">
-        <h3 class="text-muted prj-name">
-            <span class="fa fa-users fa-2x principal-title"></span>
-            Users
-        </h3>
-    </div>
-    
-    <div class="jumbotron list-content">
-        <ul class="list-group">
-            <li class="list-group-item text-left">
-                <label class="name">
-                    Juan guillermo cuadrado
-                </label>
-                <label class="pull-right">
-                    <a  class="btn btn-success btn-xs glyphicon glyphicon-ok" href="usermodify.php" title="View">Edit</a>
-                    <a  class="btn btn-danger  btn-xs glyphicon glyphicon-trash" href="#" title="Delete">Delete</a>
-                </label>
-                <div class="break"></div>
-            </li>
-        </ul>
-        <br>
-        <a class="btn btn-success btn-xs glyphicon glyphicon-ok" href="usercreation.php" title="View">Add user</a>
+<?php require 'view/components/info_to_user.php' ?>
+<div class="root">
+    <?php require 'view/components/sidebar.php' ?>
+    <div class="container bootstrap snippets bootdey">
+        <div class="header">
+            <h3 class="text-muted prj-name">
+                <span class="fa fa-users fa-2x principal-title"></span>
+                Users
+            </h3>
+        </div>
+
+        <div class="jumbotron list-content">
+            <ul class="list-group">
+                <li class="list-group-item text-left">
+                    <label class="name">
+                        Juan guillermo cuadrado
+                    </label>
+                    <label class="pull-right">
+                        <a class="btn btn-success btn-xs glyphicon glyphicon-ok" href="usermodify.php"
+                           title="View">Edit</a>
+                        <a class="btn btn-danger  btn-xs glyphicon glyphicon-trash" href="#" title="Delete">Delete</a>
+                    </label>
+                    <div class="break"></div>
+                </li>
+            </ul>
+            <br>
+            <a class="btn btn-success btn-xs glyphicon glyphicon-ok" href="usercreation.php" title="View">Add user</a>
+        </div>
     </div>
 </div>
 
